fcc1e1954eadc7d509ecd86e023cabbe39147432
Martti Alasaarela <martti.alasaarela@gmail.com>
2020-11-16 16:09:58 +0200

    Hienosäätöjä otto.php

--- RestApi-master/application/models/Otto_model.php
+++ RestApi-master/application/models/Otto_model.php
@@ -7,10 +7,9 @@ class Otto_model extends CI_model
   
   function Otto_call($add_data){
     $sql = 'Call Otto(?,?,@viesti)';
-    $query = $this->db->query($sql, $add_data);
+    $this->db->query($sql, $add_data);
     $query = $this->db->query("Select @viesti as viesti");
-    $query = $query->result_array();
-    $query = $query[0]['viesti']; 
-    return $query;
+    $result = $query->result_array();
+    return $result[0]['viesti'];
 }
 }
